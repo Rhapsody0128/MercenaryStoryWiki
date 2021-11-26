3ff424b1a618b80b76c13a5328b39d8efdda274e
Rhapsody0128 <rhapsody0128@gmail.com>
2021-11-26 11:06:38 +0800

    fix

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
 .favorite
-  .row
-    v-sheet.p.mt.rounded-lg(v-for='(heros,index) in herosList' elevation="10" color='rgba(0,0,0,0)' )
+  .row(v-for='(heros,index) in herosList')
+    v-sheet.p.mt.rounded-lg(v-if='heros.length>0' elevation="3" color='rgba(0,0,0,0)')
       div
         span.title 隊伍{{index+1}}
         svg(:class='"favoriteTpye" + (index+1)' xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24")      
           path(d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z")
       .cardList
         heroCard(:data='hero' :showSkill='true'  v-for='hero in heros' )
+
 </template>
 <script>
 export default {
